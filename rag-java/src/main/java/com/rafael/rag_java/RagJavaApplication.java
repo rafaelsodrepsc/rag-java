aed8dd9955a4800618949b1ee726890644755a8f
Rafael <rafaelsodrepsc@gmail.com>
2026-04-19 21:04:15 -0300

    feat: initial project setup

---
+++ rag-java/src/main/java/com/rafael/rag_java/RagJavaApplication.java
@@ -0,0 +1,13 @@
+package com.rafael.rag_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RagJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RagJavaApplication.class, args);
+	}
+
+}
---
+++ rag-java/src/test/java/com/rafael/rag_java/RagJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafael.rag_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RagJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
